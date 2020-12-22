bb35ec5ecfe75970aee2e7a4019ca00e2c417330
xiaotiandada <952822399@qq.com>
2020-12-22 23:44:42 +0800

    feat: rename

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import { isArray } from 'lodash';
  * 数据格式化 [ [{}], [{}], [{}] ] ===> [ {} ]
  * @param arr 需要处理的数组
  */
-export const convertToArray = (arr: any[]) => {
+export const convertArray = (arr: any[]) => {
   if (isArray(arr[0])) {
     return [].concat(...arr);
   }
--- test/test.ts
+++ test/test.ts
@@ -1,10 +1,10 @@
 import * as assert from 'assert';
-import { convertToArray } from '../src/index';
+import { convertArray } from '../src/index';
 
-describe('convertToArray', function() {
+describe('convertArray', function() {
   it('二维数组转换为一维数组', function() {
     assert.deepEqual(
       [{a: 1}, {a: 2}],
-      convertToArray( [ [{a: 1}], [{a: 2}] ] ))
+      convertArray( [ [{a: 1}], [{a: 2}] ] ))
   });
 });
