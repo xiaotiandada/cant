751457758e57dc77289a90f11050ea712550c6e2
xiaotiandada <istianlei@qq.com>
2021-05-23 20:06:00 +0800

    matchStr

--- src/index.ts
+++ src/index.ts
@@ -10,3 +10,11 @@ export const convertArray = (arr: any[]) => {
   }
   return arr;
 };
+
+
+// ------ REG --------
+// match 不包含 AB 的字符
+export const matchStr = (str: string, left: string, right: string) => {
+  const pattern = new RegExp(`(?<=${left}).*?(?=${right})`);
+  return str.match(pattern);
+};
